d977aee032a46d4c4ac5b4f6d0bf28dccb1a42b1
FilippoAdami <elpippez2@gmail.com>
2024-02-19 19:13:47 +0100

    init comments

--- sketch_feb18a/sketch_feb18a.ino
+++ sketch_feb18a/sketch_feb18a.ino
@@ -2,65 +2,69 @@
 #include <VL53L0X.h>
 #include "spider_sense.h"
 
-#define VIBRATION_BUTTON_1_PIN P2_4 // Port 6, Pin 1
-#define BEEPER_PIN P6_7 // Port 6, Pin 1
-#define BUTTON_PIN P1_1
+#define VIBRATION_BUTTON_1_PIN P2_4  // vibration button 1
+#define VIBRATION_BUTTON_2_PIN P6_7  // vibration button 2
+#define BUTTON_PIN P1_1              // start button
+#define BUTTON_SHT P1_4              // on/off button
 
-#define SHT_LOX1 P2_6
-#define SHT_LOX2 P2_7
+#define SHT_LOX1 P2_6                // lidar sensor 1
+#define SHT_LOX2 P2_7                // lidar sensor 2
 
-
+//define instances for the lidar sensors
 VL53L0X sensor0;
 VL53L0X sensor1;
 
 
-//State Machine Declaration
+//State Machine init
 State_t cur_state = STATE_WAIT;
 
-void fn_StateWait(void);
-void fn_StateInitHeight(void);
-void fn_StateWork(void);
-void menageBeeper(int level);
+void fn_StateWait(void);        // the device waits for the start button to be pressed
+void fn_StateInitHeight(void);  // the device starts polling the lidar and computes the standard height of the device
+void fn_StateWork(void);        // the device is working as 
 
+void menageBeeper(int level);   // function to manage the beeper sound at hardware level
+
+// State Machine declaration
 StateMachine_t StateMachine[] = {
 {STATE_WAIT, fn_StateWait},
 {STATE_INIT_HEIGHT, fn_StateInitHeight},
 {STATE_WORK, fn_StateWork},
 } ;
 
-void run(void);
+void run(void); // start the whole system
 
-// derived information
-const unsigned sampling_intervall = 2000;
-double height = 1700;
-
+const unsigned sampling_intervall = 2000; // value in mm of maximum distance sensed
+double height = 1700;                     // value in mm of average use height
 
 
 void setup() {
-    delay(400);
-    Serial.begin(9600);      // Initialize serial communication 
-    while ( !Serial );
-    delay(5000);
+    delay(400);            // wait 0.4 s
+    Serial.begin(9600);    // Initialize serial communication 
+    while ( !Serial );     // wait for the serial communication to activate properly
+    delay(5000);           // wait 5 seconds to allow processing of the height
     Serial.println("/////////////////SETUP/////////////////");
     
     Wire.begin();
 
+    // initialize the pins modes
     pinMode(VIBRATION_BUTTON_1_PIN, OUTPUT);
     pinMode(BEEPER_PIN, OUTPUT);
     pinMode(BUTTON_PIN, INPUT_PULLUP);
-   
-    pinMode(SHT_LOX1, OUTPUT); pinMode(SHT_LOX2, OUTPUT); 
-    digitalWrite(SHT_LOX1, LOW); digitalWrite(SHT_LOX2, LOW); 
-
+    pinMode(SHT_LOX1, OUTPUT); 
+    pinMode(SHT_LOX2, OUTPUT); 
+    
+    // turn off the lidars to allow re-addressing
+    digitalWrite(SHT_LOX1, LOW); 
+    digitalWrite(SHT_LOX2, LOW); 
+    
+    // lidar 1 addressing procedure
     delay(500);
-
-    //power LOX1 and set adress
-    digitalWrite(SHT_LOX1,HIGH);
+    digitalWrite(SHT_LOX1,HIGH);  // turn on the first lidar
     delay(100);
 
     if(sensor0.init()){
       delay(100);
-      sensor0.setAddress(0x30);
+      sensor0.setAddress(0x30);   // set the address of the first lidar
       delay(100);
       Serial.print("Sensor 0 init with I2C adress (0x");
       Serial.print( sensor0.getAddress(), HEX);
@@ -68,16 +72,15 @@ void setup() {
     }else{
       Serial.println("Sensor Not Init");
     }
-  
+    
+    // lidar 2 addressing procedure
     delay(500);
-
-    //power LOX2 and set adress
-    digitalWrite(SHT_LOX2,HIGH);
+    digitalWrite(SHT_LOX2,HIGH);   // turn on the second lidar
     delay(100);
 
     if(sensor1.init()){
       delay(100);
-      sensor1.setAddress(0x32);
+      sensor1.setAddress(0x32);;   // set the address of the first lidar
       delay(100);
       Serial.print("Sensor 1 init with I2C adress (0x");
       Serial.print( sensor1.getAddress(), HEX);
@@ -86,16 +89,18 @@ void setup() {
       Serial.println("Sensor1 Not Init");
     }
 
+    ???????????
     delay(100);
     digitalWrite(SHT_LOX1,HIGH); digitalWrite(SHT_LOX2,HIGH);
     delay(500);
-
+    ???????????
+    
     // Set measurement timing budget to a higher value for long-range mode
     sensor0.setTimeout(50);
-    sensor0.setMeasurementTimingBudget(66000); // Adjust this value as needed
+    sensor0.setMeasurementTimingBudget(66000);
 
     sensor1.setTimeout(50);
-    sensor1.setMeasurementTimingBudget(66000); // Adjust this value as needed
+    sensor1.setMeasurementTimingBudget(66000);
 
     // Set the sensor to long-range mode
     sensor0.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18); // 18 is the recommended value for long-range mode
@@ -105,8 +110,9 @@ void setup() {
     sensor1.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18); // 18 is the recommended value for long-range mode
     sensor1.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14); // 14 is the recommended value for long-range mode
 
-    sensor0.startContinuous();
-    sensor1.startContinuous();
+    // start the lidars in continuous mode
+    sensor0.startContinuous();  
+    sensor1.startContinuous(); 
 
     delay(5000);
 }
